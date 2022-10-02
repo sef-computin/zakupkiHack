51cdc153aa57de1c71934999d9ef0d3c061d3fcc
sef-computin <inch.bdg@gmail.com>
2022-10-02 11:40:35 +0300

    подгон резов 11:40

--- Contracts/views.py
+++ Contracts/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contracts
+
+import pandas as pd
+import json
 # Create your views here.
 
 def index(responce):
-    return render(responce, 'index.html')
+    contracts = Contracts.objects.all()[:5]
+    for contract in contracts: print(contract.product_name)
+    # return render(responce, 'index.html')
+    return render(responce, 'index.html', {'contracts': contracts})
+
+# Create your views here.
+def table(request):
+    df = pd.read_csv("~/Проекты/rosseltorg/big_data/norm_data.csv")
+    # parsing the DataFrame in json format.
+    json_records = df.reset_index().to_json(orient ='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d': data}
+  
+    return render(request, 'table.html', context)
