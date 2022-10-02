0e3053f55e31a3940bc3e83bdcc13ce9c879e250
sef-computin <inch.bdg@gmail.com>
2022-10-02 10:14:57 +0300

    Ласт день 10:14

--- Contracts/models.py
+++ Contracts/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Contracts(models.Model):
 
-    product_name = models.TextField(blank=True, primary_key=True)
+    id = models.IntegerField(blank=True, primary_key=True)
+    product_id = models.IntegerField(blank=True, null=True)
+    product_name = models.TextField(blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
     product_vat_rate = models.TextField(blank=True, null=True)
     product_msr = models.TextField(blank=True, null=True)
@@ -11,7 +13,8 @@ class Contracts(models.Model):
     okpd2_code = models.TextField(blank=True, null=True)
     okpd2_name = models.TextField(blank=True, null=True)
     inn = models.TextField(blank=True, null=True)
-    country_code = models.TextField(blank=True, null=True)
+    country_code = models.IntegerField(blank=True, null=True)
+    country_name = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Contract'
